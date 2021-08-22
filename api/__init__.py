96e03615b5aa8a3ee305128d720f4dd4ea8d87ff
boo <booblegum42@gmail.com>
2021-08-22 05:23:29 -0700

    setting for heroku

--- api/__init__.py
+++ api/__init__.py
@@ -9,6 +9,6 @@ api = Api(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-path = os.path.dirname(os.path.abspath(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(path, 'test.db')
+base_dir = os.path.dirname(os.path.abspath(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(base_dir, 'test.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
