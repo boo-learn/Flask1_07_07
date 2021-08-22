2fac0bdb1bb0e4ae93b36a74bf928c7a863909d4
boo <booblegum42@gmail.com>
2021-08-22 06:00:21 -0700

    new migrate + fix path DB

--- api/__init__.py
+++ api/__init__.py
@@ -1,4 +1,5 @@
 import os
+# import pathlib
 from flask import Flask
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
@@ -9,6 +10,9 @@ api = Api(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-base_dir = os.path.dirname(os.path.abspath(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL').replace("://", "ql://", 1) or 'sqlite:///' + os.path.join(base_dir, 'test.db')
+base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if os.environ.get('DATABASE_URL'):
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(base_dir, 'test.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
